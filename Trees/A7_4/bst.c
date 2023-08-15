fd1cb44944c040955e31dc92d8611ed2ff0a0bc9
Rayaan Khan <rayaan.k.ca@gmail.com>
2023-08-15 01:00:45 -0400

    the last commit

--- Trees/A7_4/bst.c
+++ Trees/A7_4/bst.c
@@ -6,174 +6,30 @@
 
 TNODE *search(TNODE *root, char *name) {
     
-    // your implementation | Binary Search Option, recursive
+  // your implementation | Binary Search Option, recursive
 
-    // recursive method
-    // check if the root is a match
-    if (strcmp(root->data.name,name) == 0) { 
-      return root;
-    } 
+  if (root == NULL || strcmp(root->data.name, name) == 0) {
+    return root;
+  }
 
-    else if (strcmp(root->data.name,name) < 0) {
-      return search(root->left, name);
-    } else {
-      return search(root->right, name);
-    }
+  if (strcmp(root->data.name, name) < 0) {
+    return search(root->data, name)
+  }
+
+  else {
+    return search(root->left, name)
+  }
+
 
 }
 
 void insert(TNODE **rootp, char *name, float score) {
     
-    TNODE *node = new_node(name, score); // Create a new node with the given name and score
 
-    if (!*rootp) { // If the root pointer is empty, i.e., the tree is empty
-        *rootp = node; // Set the root pointer to the new node
-        return; // Exit the function
-    }
-
-    TNODE *current = *rootp; // Start from the root of the tree
-
-    while (current) { // Traverse the tree until a suitable position is found
-        
-        // if name is alphabetically less than node move to left subtree
-        if (strcmp(name, current->data.name) < 0) { 
-            
-            // if current does not contain a left child, insert new node here
-            if (!current->left) { 
-                current->left = node; 
-                return; // Exit the function
-            }
-            current = current->left; // Move to the left child
-        } 
-        
-        // if name is alphabetically less than node move to left subtree
-        else if (strcmp(name, current->data.name) > 0) { // If the name is greater than the current node's name
-            
-            // if current does not contain a right child, insert new node here
-            if (!current->right) { 
-                current->right = node; // Set the right child to the new node
-                return; // Exit the function
-            }
-            current = current->right; // Move to the right child
-        } 
-        
-        
-        else { // If the names are equal
-            current->data.score = score; // Update the score of the current node
-            return; // Exit the function
-        }
-    }
 }
 
 void delete(TNODE **rootp, char *name) {
     
-    // nothing to delete
-    if (!*rootp) {
-      return;
-    }
-
-    // keep track of the parent and cur node
-    TNODE *parent = NULL;
-    TNODE *cur = *rootp;
-
-    while (cur) {
-
-      // if name is larger. move to right sub-tree
-      if (strcmp(name, cur->data.name) > 0) {
-        parent = cur;
-        cur = cur->right;
-        continue;
-      }
-
-      // if name is smaller. move to left sub-tree
-      else if (strcmp(name, cur->data.name) < 0) { 
-        parent = cur;
-        cur = cur->left;
-        continue;
-      }
-
-      // match found
-      else {
-
-        // scenario 1: match has no children
-        if (cur->left == NULL && cur->right == NULL) {
-          
-          // if no parent (means tree only has one node)
-          if (!parent) {
-            *rootp = NULL;
-          }
-
-          // has parents but no children
-          else {
-
-            if (parent->left == cur) {
-              parent->left = NULL;
-            } else {
-              parent->right = NULL;
-            }
-          }
-
-        // scenario 2: match has one child
-        } else if ((cur->left) != (cur->right)) {
-
-          // determine the child node
-          TNODE *child;
-          if (cur->left == NULL) {
-            child = cur->right;
-          } else {
-            child = cur->left;
-          }
-
-          // if it has no parent
-          if (!parent) {
-            *rootp = child;
-          }
-
-          // has a parent and one child
-          else {
-
-            if (parent->left == cur) {
-              parent->left = child;
-            } else {
-              parent->right = child;
-            }
-            free(cur);
-          }
-
-        }
-
-        // scenario 3: has two children
-        TNODE *minNode = extract_smallest_node(&cur);
-
-        // if the left child is the minimum of the two
-        if (cur->right == minNode) {
-          minNode->left = cur->left;
-          minNode->right = cur->right;
-        }
-
-        // if the right hcild is the minimum of thw two
-        else {
-          minNode->left = cur->left;
-          minNode->right = cur->right;
-        }
-
-        // no parent
-        if (!parent) {
-          *rootp = minNode;
-        } else {
-            if (parent->left == cur) {
-                parent->left = minNode;
-            } else {
-                parent->right = minNode;
-            }
-        }
-        free(cur);
-        cur = NULL; // Prevent dangling pointer
-
-
-      }
- 
-    }
 
 }
 
